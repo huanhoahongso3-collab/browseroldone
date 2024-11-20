80b4fcff84231cb9439045ffaa07bda8e4c11029
huanhoahongso3-collab <144003289+huanhoahongso3-collab@users.noreply.github.com>
2024-11-20 19:43:34 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-browservice-v0.9.10.0-x86_64.AppImage%0D --appimage-extract-and-run --vice-opt-http-listen-addr=0.0.0.0:80
+browservice.AppImage --appimage-extract-and-run --vice-opt-http-listen-addr=0.0.0.0:80
