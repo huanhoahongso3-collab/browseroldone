e8b29130484b71812771b1dd2f1b2878a02d28d3
huanhoahongso3-collab <144003289+huanhoahongso3-collab@users.noreply.github.com>
2024-11-20 19:53:31 +0700

    Create wine.sh

---
+++ wine.sh
@@ -0,0 +1,7 @@
+sudo dpkg --add-architecture i386
+cat /etc/os-release
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
+sudo apt update
+sudo apt install --install-recommends winehq-stable
