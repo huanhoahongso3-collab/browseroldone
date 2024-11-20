792fe8acab5fbbb1ac9099ad91c3e4078b20178e
huanhoahongso3-collab <144003289+huanhoahongso3-collab@users.noreply.github.com>
2024-11-20 16:49:30 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+@echo off
 wget https://github.com/ttalvitie/browservice/releases/download/v0.9.10.0/browservice-v0.9.10.0-x86_64.AppImage
 chmod +x browservice-v0.9.10.0-x86_64.AppImage
 ./browservice-v0.9.10.0-x86_64.AppImage --vice-opt-http-listen-addr=0.0.0.0:80
