c5045908c2d3db4aef1f24a69c1f85d2b60fcd93
huanhoahongso3-collab <144003289+huanhoahongso3-collab@users.noreply.github.com>
2024-11-20 19:55:46 +0700

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -1,8 +1,8 @@
 sudo dpkg --add-architecture i386
 cat /etc/os-release
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
-sudo apt update
-sudo apt install --install-recommends winehq-stable
+mkdir -pm755 /etc/apt/keyrings
+wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/focal/winehq-focal.sources
+apt update
+apt install --install-recommends winehq-stable
 wget
