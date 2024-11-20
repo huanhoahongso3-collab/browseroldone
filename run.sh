77ea14b437a3f642193aaac6b73a7d645cd37c32
huanhoahongso3-collab <144003289+huanhoahongso3-collab@users.noreply.github.com>
2024-11-20 17:01:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 wget https://download1526.mediafire.com/vrngzqpvt1wg0iBc9s-Q44iwyZeIKxaPIg12Cm7zONlnEZXJhbI3sKYSVZ0Ul0vDr-hI2FHyjq0Y0X3UN7lru4hEK5DzsYzjh9xdJ2Phij3O_rUsW0zietDgyz6-DSzzTV0OCG1z1iJwebJy6g356i3K9l7A0GXXD4adz3X-66OgcME/va8ld8jjftivxmm/browservice-v0.9.10.0-x86_64.AppImage
 chmod +x browservice-v0.9.10.0-x86_64.AppImage
-./browservice-v0.9.10.0-x86_64.AppImage 
+./browservice-v0.9.10.0-x86_64.AppImage --vice-opt-http-listen-addr=0.0.0.0:80
